3eef33f549f2347141a3dd5140eff09254a2cc4d
PedroBritoWR <pedrowpvh@hotmail.com>
2024-10-22 13:03:51 -0400

    Criando DTO

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -2,12 +2,12 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,8 +18,9 @@ public class FoodController {
     private FoodRepository repository;
 
     @GetMapping
-    public void getAll(){
-        List<Food> foodList = repository.findAll();
-//        return foodList;
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 }
